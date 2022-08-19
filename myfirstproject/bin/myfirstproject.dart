99769cdf4ab5db5634895dee820fb98fca755153
Eugene <67829024+EugeneMuzyka@users.noreply.github.com>
2022-08-19 21:12:26 +0300

    Add files via upload

---
+++ myfirstproject/bin/myfirstproject.dart
@@ -0,0 +1,28 @@
+import 'dart:html';
+
+void main() {
+   //Первое задание 
+  // НОД целых чисел
+int gcd(int a, int b);
+
+// НОК целых чисел
+int lcm(int a, int b);
+
+// Простые множители числа
+List<int> factors(int number);
+  //Второе задание
+  1) String intToBinaty(int number);
+  2) Int binaryToInt(String binary);
+  //третье задание
+List<num>findNumsIn(String string);
+//четвертое задание
+Map<String,int>findStringsIn(List<String>stringList);
+//пятое задание
+Set<int>findNumNamesIn(List<String>StringList);
+//Шестое задание
+Point(x, y, z);
+//Седьмое задание
+num.rootPower(x);
+//Восьмое задание
+List<String> getEmails();
+}
